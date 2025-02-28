4a27455a078201677d72ad711c8423f1eb6c42e2
juanfriorozco8 <oro24647@uvg.edu.gt>
2025-02-27 21:12:46 -0600

    ArayList

--- ArrayList.java
+++ ArrayList.java
@@ -1,3 +1,58 @@
-public class ArrayList {
-    
+
+/**
+ * Implementation of a stack using an ArrayList.
+ * @param <T> type of elements stored in the stack.
+ */
+public class ArrayList<T> implements IStack<T> {
+
+    private ArrayList<T> stack; // ArrayList to store the elements of the stack.
+
+    /**
+     * Constructor of the stack.
+     */
+    public ArrayListStack() {
+        this.stack = new ArrayList<>();
+    }
+
+    /**
+     * Adds an element to the top of the stack.
+     * @param value element to be added.
+     */
+    @Override
+    public void push(T value) {
+        stack.add(value);
+    }
+
+    /**
+     * Removes the top element of the stack.
+     * @return the removed element or null if the stack is empty.
+     */
+    @Override
+    public T pop() {
+        if (isEmpty()) {
+            return null; // If the stack is empty, return null.
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    /**
+     * Checks if the stack is empty.
+     * @return true if the stack is empty, false otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    /**
+     * Evaluates a postfix expression.
+     * @param value the postfix expression to be processed.
+     * @return the result of the evaluation.
+     */
+    @Override
+    public T postfixExpression(T value) {
+        
+        return value;
+    }
 }
+
